783681e9cd662914f12caee5a4607c1b91417c1c
Kevin <kkerr87@gmail.com>
2012-03-19 13:16:45 -0400

    lexer now working correctly

--- xmlparser.c
+++ xmlparser.c
@@ -5,6 +5,7 @@
 #include <ctype.h>
 #include <string.h>
 #define MAXVAL 10000
+#define HASHSIZE 101
 #define TRUE 1
 #define FALSE 0
 #define INVAL 100000
@@ -12,9 +13,92 @@
 #define R_ARROW 129
 #define QUOTE 130
 #define STRING 131
-
+#define LINK 132
+#define CLINK 133
+#define TITLE 134
+#define CTITLE 135
+#define DESCRIPTION 136
+#define CDESCRIPTION 137
+#define COMMENT 138
+#define CCOMENT 139
+#define OTHER 140
 
 char input[INVAL];
+
+struct nlist { // the symbol table
+	struct nlist *next;
+	char *name;
+	char *defn;
+};
+
+static struct nlist *hashtab[HASHSIZE];
+
+// hash forms a hash value for look up and install
+unsigned hash(char *s) {
+	unsigned hashval;
+	
+	for (hashval = 0; *s != '\0'; s++) 
+		hashval = *s + 31 * hashval;
+	return hashval % HASHSIZE;
+}
+
+struct nlist  *lookup(char *s) {
+	struct nlist *np;
+
+	for (np = hashtab[hash(s)]; np!=NULL; np = np->next)
+		if (strcmp(s, np->name) == 0)
+			return np; 
+		return NULL;
+}
+
+struct nlist *install(char *name, char *defn) {
+	
+	struct nlist *np;
+	unsigned hashval;
+
+	if ((np=lookup(name))==NULL) {
+		np = (struct nlist *) malloc(sizeof(*np));
+		if (np == NULL || (np->name = strdup(name)) == NULL)
+			return NULL;
+		hashval = hash(name);
+		np->next = hashtab[hashval];
+		hashtab[hashval] = np;
+	} else if ((np=lookup(name))!=NULL) 
+		free ((void *) np->defn);
+	if ((np->defn = strdup(defn)) == NULL)
+		return NULL;
+	return np;
+}
+
+void inithash(void) {
+	struct nlist *hlink;
+	struct nlist *htitle;
+	struct nlist *hdescription;
+	struct nlist *hclink;    // c means closed, i.e we want a "closed title" token type.
+	struct nlist *hctitle;
+	struct nlist *hcdescription;
+	struct nlist *hcomment;
+	struct nlist *hccomment;
+	
+	char *link = "132";
+	char *title = "134";
+	char *description = "136";
+	char *clink = "133";
+	char *ctitle = "135";
+	char *cdescription = "137";
+	char *comment = "138";
+	char *ccomment = "139";
+
+
+	hlink = install("<link>", link);
+	htitle = install("<title>", title);
+	hdescription = install("<description>", description);
+	hclink = install("</link>", clink);
+	hctitle = install("</title>", ctitle);
+	hcdescription = install("</description>", cdescription);	
+	hcomment = install("<comments>", comment);
+	hccomment = install("</comments>", ccomment);
+}
 
 typedef struct token {
 	int *type;
@@ -30,7 +114,7 @@ struct Lexer {
 struct Lexer lexer; 
 
 int issymbol(char c) {
-	if ((c=='/') || (c == '&') || (c == '(') || (c == ')') || (c == '[') || (c == ']') || (c == '!') || (c == '?'))
+	if ((c == '<') || (c == '>') || (c == '"')) 		
 		return 1;
 	else
 		return 0;
@@ -73,21 +157,41 @@ void GetInput(void) {
 	
 int GetDifference(void) {
 	int diff;
-	
-	if ((*lexer.end == '<') || (*lexer.end == '>'))
-		++lexer.end;
-	else if ((isalnum(*lexer.end)) || (*lexer.end == '"') || (*lexer.end == '/') || (*lexer.end == ']') || (*lexer.end == '[') || (*lexer.end =='!') || (*lexer.end == '(')) {
-		while (((*lexer.end!='<') && (*lexer.end!='>')) && (*lexer.end!='\0'))
+
+	if (*lexer.end=='<') {
+		while (*lexer.end!='>')
 			++lexer.end;
-	}
-	diff = lexer.end - lexer.start;
-	return diff;
+		diff = lexer.end - lexer.start;
+		return diff;
+	} else if (*lexer.end!='<') {
+		while ((*lexer.end!='<') && (*lexer.end!=EOF))
+			++lexer.end;
+		diff = lexer.end - lexer.start;
+		return diff;
+    } else 
+		fprintf(stderr, "GET DIFFERENCE ERROR!\n");
 }
 	
+	
 void resetInput(void) {	
-	lexer.start = lexer.end; 
+ 
 	if (*lexer.end == '\0')
 		;
+	else if (*lexer.end == '\n') {
+		++lexer.end;
+		lexer.start = lexer.end;
+  } else if (*lexer.end == '<')
+		lexer.start = lexer.end;
+	else if (*lexer.end == '>') { //move lexer.end onto the first character of the next string
+		++lexer.end;
+		if (*lexer.end == '\n') {
+			++lexer.end;
+			lexer.start = lexer.end;
+			}
+		else
+			lexer.start = lexer.end;
+}
+		
 	else {
 		while ((*lexer.start!='\0') && junk(*lexer.start))		
 			++lexer.start;
@@ -98,14 +202,28 @@ void resetInput(void) {
 Token *GetToken(void) {
 	int diff = GetDifference();
 	Token *p = malloc(sizeof(Token));
-	
+	struct nlist *look;
+
 	if (*lexer.start == '<') {
+		p->type = malloc(sizeof(int));
+		p->value = malloc((sizeof(char) * diff) + 2);
+		strncpy(p->value, lexer.start, (++diff));
+		p->value[diff + 1] = '\0';
+		if((look=lookup(p->value))!=NULL)
+			*p->type = atoi(look->defn);
+		else
+			*p->type = OTHER;
+    }
+		
+		
+	
+/*	if (*lexer.start == '<') {
 		p->type = malloc(sizeof(int));
 		*p->type = L_ARROW;
 		p->value = malloc((sizeof(char) * diff) + 1);
 		strncpy(p->value, lexer.start, diff);
-		p->value[diff + 1] = '\0';
-}
+		p->value[diff + 1] = '\0'; */
+
 	else if (*lexer.start == '>') {
 		p->type = malloc(sizeof(int));
 	    *p->type = R_ARROW;
@@ -121,7 +239,7 @@ Token *GetToken(void) {
 		strncpy(p->value, lexer.start, diff);
 		p->value[diff + 1] = '\0';
 }
-	else if ((isalnum(*lexer.start)) || (issymbol(*lexer.start))) {
+	else if ((isalnum(*lexer.start)) || (!issymbol(*lexer.start))) {
 		p->type = malloc(sizeof(int));
 		*p->type = STRING;
 		p->value = malloc((sizeof(char) * diff) + 1);
@@ -131,6 +249,7 @@ Token *GetToken(void) {
 	else {
 		fprintf(stderr, "GET TOKEN ERROR");
 }		
+
 	 
 	resetInput();
 	return p;			
@@ -149,6 +268,7 @@ void printString(Token *p) {
 
 main()  {
 GetInput();
+inithash();
 init_lexer();
 while(*lexer.start!='\0') {
 
