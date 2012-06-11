7e22e43f5faba7c42e09114b89e6b27348a2ada9
Kevin <kkerr87@gmail.com>
2012-06-11 09:06:19 -0400

    Updated README, added license

---
+++ README.md
@@ -0,0 +1,11 @@
+# HNREADER: Read HackerNews From Your Terminal #
+
+### How To Use ###
+
+To build, make sure that W3M is installed on your machine and use Make:
+
+		make hnreader
+
+Running hnreader from the commandline will produce a stream of the latest stories posted on HackerNews.  To move to the next post, press either 'n' or enter.  To see the comments for a post, press 'c', and to see the story itself press 's'.
+
+
--- hnreader.c
+++ hnreader.c
@@ -1,5 +1,18 @@
-// this is a special-case xml parser for xml returned from hacker news
-// if you use this for anything else it probably wont' work
+/*HNREADER: Hacker News Terminal Reader
+Copyright (C) 2012 Kevin Kerr
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
